1ae0e59b7bf6c44eca495b345b1e29952ccee2d2
ZabiKe <keller-lukas@gmx.de>
2021-09-23 22:44:23 +0200

    first commit

---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa1xrg9uxaxXXNTw5d7',
+        issuer: 'https://dev-98549017.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile','email']
+    }
+}
